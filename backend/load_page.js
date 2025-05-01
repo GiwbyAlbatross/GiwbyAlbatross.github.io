e1519d558b95eab866b4dce7c2050ca6a81a0b54
Giwby Albatross <giwby.albatross@gmail.com>
2025-05-01 14:23:48 +1000

    Continue to develop load_page.js
    I’ll keep changing it (`load_page.js`) until it does what I want it to. I’m not sure what that is however…

--- backend/load_page.js
+++ backend/load_page.js
@@ -3,13 +3,15 @@
 const src_loc = document.location.origin +'/pages/'+ document.location.pathname.split('/').at(-1)+'.html';
 
 // DEBUG
-window.alert("Requesting page at " + src_loc)
+console.log("Requesting page at " + src_loc)
 
 const req = XMLHttpRequest();
 
 function actuallyLoadThePage(e) {
     if (req.responseCode == 404) {
         req.open("GET", document.location.origin + '/pages/404.html');
+    } else if (req.responseCode != 200) {
+        document.writeln("Request suffered HTTP response code: " + req.responseCode.toString());
     } else {
         document.writeln(req.responseText);
     }
